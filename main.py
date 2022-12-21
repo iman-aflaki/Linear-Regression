65d2fc6df31104cbbeba93e5bc94586b3afdf6d9
iman aflaki <113721468+iman-aflaki@users.noreply.github.com>
2022-12-21 11:44:00 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+data = pd.read_csv('headbrain.csv')
+X = data['Head Size(cm^3)'].values
+Y = data['Brain Weight(grams)'].values
+# mean of X and Y
+mean_x = data['Head Size(cm^3)'].mean()
+mean_y = data['Brain Weight(grams)'].mean()
+# total number of rows
+n = len(X)
+numerator = 0
+denuminator = 0
+for i in range(n):
+    numerator = numerator + ((X[i] - mean_x)*(Y[i] - mean_y))
+    denuminator = denuminator + (X[i] - mean_x)**2
+m = numerator / denuminator
+c = (mean_y - (m * mean_x))
+min_x = np.min(X)
+max_x = np.max(X)
+x = np.linspace(min_x, max_x)
+y = (m * x) + c
+plt.plot(x, y, color='red', label='linear regression', linewidth=3)
+plt.scatter(X, Y, color='blue', label='Scatter Plot', alpha=0.5)
+plt.xlabel('Head size (cm^3)')
+plt.ylabel('Brain weight(grams)')
+plt.legend()
+plt.show()
+
